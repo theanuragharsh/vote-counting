a11a33927a8e2000be190a7ba755f176a1fdb42c
theanuragharsh <nrgharsh@gmail.com>
2022-12-12 12:43:53 +0530

    VoteCounterRunnable service developed and deployed to master branch

--- src/main/java/com/threadingbasics/VoteCounterRunnable.java
+++ src/main/java/com/threadingbasics/VoteCounterRunnable.java
@@ -1,2 +1,27 @@
-package com.threadingbasics;public class VoteCounterRunnable {
+package com.threadingbasics;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VoteCounterRunnable implements Runnable {
+    Integer counter = 0;
+
+    public Integer getCounter() {
+        return counter;
+    }
+
+    public void setCounter(Integer counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println(Thread.currentThread().getName() + " : Initial count : " + getCounter());
+        synchronized (this) {
+            setCounter(getCounter() + 1);
+            System.out.println("/n calculating count .. ");
+        }
+        System.out.println(Thread.currentThread().getName() + " : Final Count : " + getCounter());
+    }
 }
