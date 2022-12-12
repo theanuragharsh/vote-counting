5fe0ac52ffbfa628c0138c47824a576d943abba8
theanuragharsh <nrgharsh@gmail.com>
2022-12-12 12:29:49 +0530

    Initial commit | Pushing repo to git

---
+++ src/main/java/com/threadingbasics/VoteCountingApplication.java
@@ -0,0 +1,13 @@
+package com.threadingbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteCountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteCountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/threadingbasics/VoteCountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threadingbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteCountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
